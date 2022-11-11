d35d633700f88198e55430b151ad222c5d75cd0c
Mikael_Gienson <juchniewicz.m@gmail.com>
2022-11-11 17:50:36 +0100

    Done mocking async fn

---
+++ script2.js
@@ -0,0 +1,29 @@
+const fetch = require("node-fetch");
+
+const getPeoplePromise = (fetch) => {
+  return fetch("http://swapi.dev/api/people/")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      return {
+        count: data.count,
+        results: data.results,
+      };
+    });
+};
+
+const getPeople = async (fetch) => {
+  const getRequest = await fetch("http://swapi.dev/api/people/");
+  const data = await getRequest.json();
+  return {
+    count: data.count,
+    results: data.results,
+  };
+};
+
+
+
+module.exports = {
+  getPeople,
+  getPeoplePromise,
+};
